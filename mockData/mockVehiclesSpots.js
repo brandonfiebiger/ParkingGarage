4cb13bc4237d6c79203296201ef2673a0a04e04b
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-04-01 00:44:40 -0600

    Add mockData

---
+++ mockData/mockVehiclesSpots.js
@@ -0,0 +1,42 @@
+mockVehiclesSpots = { '50': 
+   { id: 311,
+     size: 'small',
+     row: 1,
+     level: 1,
+     vehicle_id: 50,
+     created_at: '2019-04-01T01:45:02.033Z',
+     updated_at: '2019-04-01T01:45:02.033Z' },
+  '51': 
+   { id: 312,
+     size: 'small',
+     row: 1,
+     level: 1,
+     vehicle_id: 51,
+     created_at: '2019-04-01T01:45:02.033Z',
+     updated_at: '2019-04-01T01:45:02.033Z' },
+  '52': 
+   { id: 341,
+     size: 'small',
+     row: 5,
+     level: 2,
+     vehicle_id: 52,
+     created_at: '2019-04-01T01:45:02.049Z',
+     updated_at: '2019-04-01T01:45:02.049Z' },
+  '53': 
+   { id: 343,
+     size: 'small',
+     row: 5,
+     level: 2,
+     vehicle_id: 53,
+     created_at: '2019-04-01T01:45:02.049Z',
+     updated_at: '2019-04-01T01:45:02.049Z' },
+  '54': 
+   { id: 381,
+     size: 'medium',
+     row: 10,
+     level: 3,
+     vehicle_id: 54,
+     created_at: '2019-04-01T01:45:02.070Z',
+     updated_at: '2019-04-01T01:45:02.070Z' } }
+
+     module.exports = mockVehiclesSpots;
